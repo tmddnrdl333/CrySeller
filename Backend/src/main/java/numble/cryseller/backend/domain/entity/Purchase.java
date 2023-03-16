2689cf217518368788d4a16d04f7bd7553e00e2d
SeungNong <tmddnrdl333@naver.com>
2023-03-16 21:05:02 +0900

    fix: jpa auditing listener added

--- Backend/src/main/java/numble/cryseller/backend/domain/entity/Purchase.java
+++ Backend/src/main/java/numble/cryseller/backend/domain/entity/Purchase.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -14,6 +15,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Purchase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
